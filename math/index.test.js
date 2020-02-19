2b33f553538547dc66822f2be2fcc4c71843b087
Gratus Devanesan <gratuz@hotmail.com>
2020-02-19 14:19:40 -0500

    Adding a failing test

--- math/index.test.js
+++ math/index.test.js
@@ -3,3 +3,7 @@ const index = require('./index');
 test('adds 1 + 2 to equal 3', () => {
   expect(index.sum(1, 2)).toBe(3);
 });
+
+test('expect to fail', () => {
+  expect(index.sum(1, 2)).toBe(4);
+});
